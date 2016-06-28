306fb9517dcae3a9c55abc73ae85ab4eb1c1ce74
i709134 <lou.brevetti@voya.com>
2016-06-28 11:04:44 -0400

    text update

--- demo/demo.js
+++ demo/demo.js
@@ -7,11 +7,7 @@ function appLoaded(){
 	let menu = document.querySelector('.toolbar');
 	let voyaChart = document.querySelector('voya-chart');
 	voyaChart.api.eventBus.on('rendered',function(){
-		//let colors={}
-		//voyaChart.api.chartModel.data.columns.forEach(function(col,idx){
-		//	colors[col[0]] = "#"+brandedModel[idx];
-		//})
-		//voyaChart.api.updateColors(colors);
+		/* TODO: any updates to charts that need to be done in a non event based handler */
 	})
 	delegate(menu).on('click',"li",function(e){
 			let colors={}
