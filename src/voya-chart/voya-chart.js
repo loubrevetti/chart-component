aeb9958948c6d510f345cb973310aa00ca121082
Bob Hutchings <robert.hutchings@voya.com>
2016-08-04 14:30:03 -0400

    Made sure voya-chart can finish initializing, even if no data.
    This will insure that the chart object exists, so that data can be
    injected into the chart after the fact.

--- src/voya-chart/voya-chart.js
+++ src/voya-chart/voya-chart.js
@@ -120,8 +120,9 @@ export class VoyaChart{
 				}
 
 			} else {
-				// TODO: How should the UI indicate there is no data to display?
-				console.log('ERROR: Chart cannot be constructed. No Data.');
+				// This will allow the Chart Object to finish initializing, though nothing will be rendered on-screen.
+				this.dataModel = [];
+				console.log("WARN - Voya Chart. Unable to render chart. No chart data provided.");
 			}
 
 			// Only need to add the responsive listener, if there's some possibility of successfully rendering the chart.
